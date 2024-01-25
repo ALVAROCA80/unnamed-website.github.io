2f7c497fda315be50ac097be503455e8171fdbfd
ALVAROCA-0 <57073112+ALVAROCA80@users.noreply.github.com>
2024-01-25 13:35:19 -0500

    ufff

--- javascript/home.js
+++ javascript/home.js
@@ -1,7 +1,23 @@
-window.addEventListener("resize", function () {
-    const width = document.body.clientWidth;
-    const panels = document.querySelectorAll(".grow-panels");
-    for (const panel of panels) {
-        panel.style.setProperty("--x-margin",`${Math.floor((width%panel.offsetWidth)/(2*Math.floor(width/panel.offsetWidth)))}px`)
+let promociones;
+fetch("../javascript/promociones.json").then((res)=> {
+    res.json().then((res)=> {
+        promociones = res;
+        shuffle("promo", 0)
+    })
+})
+
+let current_Promotion = 0;
+function shuffle(name, move) {
+    current_Promotion = (current_Promotion+move)%promociones.length;
+    if (current_Promotion < 0)
+        current_Promotion = promociones.length+current_Promotion;
+    let div = document.getElementsByName(name)[0];
+    for (let child of div.children) {
+        let name = child.tagName.toUpperCase();
+        if (name == "IMG") {
+            child.src = promociones[current_Promotion]["img"];
+        } else if (name == "P") {
+            child.innerText = promociones[current_Promotion]["name"];
+        }
     }
-}, true)
+}
