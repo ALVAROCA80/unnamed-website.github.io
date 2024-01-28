1310728526da13c8a837a87190aeffd3f67f3a71
ALVAROCA-0 <57073112+ALVAROCA80@users.noreply.github.com>
2024-01-27 20:37:28 -0500

    Changes during meeteing

--- javascript/insertTopNav.js
+++ javascript/insertTopNav.js
@@ -1,13 +1,13 @@
 replace_Tag_HTML(document.querySelector("script#replace_with_navbar"), "topNav.html")
 
 document.addEventListener("scroll", function () {
-    const header = document.querySelector(".header");
+    const header = document.querySelector(".beforeNav");
     const topNav = document.querySelector(".topNav");
     const height_offset = header.offsetHeight;
     const scrolled_Distance = Math.abs(document.body.getBoundingClientRect().top);
     if (scrolled_Distance >= height_offset) {
         topNav.classList.add("fixed");
-        header.style.marginBottom = `${header.offsetHeight}px`;
+        header.style.marginBottom = `${topNav.offsetHeight}px`;
     } else {
         topNav.classList.remove("fixed");
         header.style.marginBottom = "0";
